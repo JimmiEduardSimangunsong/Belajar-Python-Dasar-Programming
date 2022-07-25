6d1fb183310ab96b931a6428faeb88e84e732018
Jimmi Eduard Simangunsong <jimmi24eduard@gmail.com>
2022-07-25 11:22:47 +0700

    Belajar-Dasar-Python

---
+++ alur program python/main.py
@@ -0,0 +1,9 @@
+print("apa kabar friends?")
+print("hello world")
+
+# alur program dieksekusi dari baris atas ke bawah
+# baris kosong dilewati
+"""komen multi line """
+#kita bisa mencompile python 
+#ke yang namanya bytecode
+#python -m py_compile main.py
---
+++ variabel/main.py
@@ -0,0 +1,8 @@
+# tidak ada deklarasi int c = 4 dll
+a = 10
+x = 5
+panjang = 1000
+
+print("nilai a =",a)
+print("nilai x =",x)
+print("nilai panjang =", panjang)
